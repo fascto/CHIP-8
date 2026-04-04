35fa3591255a2c946063b627bb514774ad7722c5
Fascto <fascto.05@gmail.com>
2026-04-04 19:00:15 +0000

    feat(decoding): Adding 0x3000 to 0x9000 cases

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -146,36 +146,137 @@ void Chip8::LoopFDE() {
         // Jump to location nnn (1nnn - JP addr)
         case 0x1000:
             PC = nnn;
-            break;
+            return;
 
         // Call subroutine at nnn (2nn - CALL addr)
         case 0x2000:
             SP++;
             stack[SP] = PC;
             PC = nnn;
-            break;
+            return;
 
+        // Skip next instruction if Vx = nn (3xkk - SE Vx, byte)
         case 0x3000:
+            if (V[x] == kk)
+                PC += 2;
             break;
 
+        // Skip next instruction if Vx != kk. (4xkk - SNE Vx, byte)
         case 0x4000:
+            if (V[x] != kk)
+                PC += 2;
             break;
 
+        // Skip next instruction if Vx = Vy (5xy0 - SE Vx, Vy)
         case 0x5000:
+
+            if (V[x] == V[y])
+                PC += 2;
             break;
 
+        // Set Vx = kk (6xkk - LD Vx, byte)
         case 0x6000:
+            V[x] = kk;
             break;
 
+        // Set Vx = Vx + kk (7xkk - ADD Vx, byte)
         case 0x7000:
+            V[x] = V[x] + kk;
             break;
 
         case 0x8000:
-            break;
+            switch (opcode & 0x000F) {
+                // Set Vx = Vy (8xy0 - LD Vx, Vy)
+                case 0x0000:
+                    V[x] = V[y];
+                    break;
 
+                // Set Vx = Vx OR Vy (8xy1 - OR Vx, Vy)
+                case 0x0001:
+                    V[x] = V[x] | V[y];
+                    break;
+
+                // Set Vx = Vx AND Vy (8xy2 - AND Vx, Vy)
+                case 0x0002:
+                    V[x] = V[x] & V[y];
+                    break;
+
+                // Set Vx = Vx XOR Vy (8xy3 - XOR Vx, Vy)
+                case 0x0003:
+                    V[x] = V[x] ^ V[y];
+                    break;
+
+                // Set Vx = Vx + Vy, set VF = carry. (8xy4 - ADD Vx, Vy)
+                case 0x0004: {
+                    const uint16_t s = (V[x] + V[y]);
+
+                    if ( s > 0xFF )
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
+
+
+                    V[x] = static_cast<uint8_t>( s & 0x00FF );
+
+                    break;
+                }
+
+                // Set Vx = Vx - Vy, set VF = NOT borrow (8xy5 - SUB Vx, Vy)
+                case 0x0005:
+
+                    if (V[x] > V[y])
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
+
+                    V[x] = V[y] - V[x];
+                    break;
+
+                // Set Vx = Vx SHR 1. (8xy6 - SHR Vx {, Vy})
+                case 0x0006:
+
+                    if ( (V[x] & 0x01) == 0x01)
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
+
+                    V[x] >>= 1;
+                    break;
+
+                // Set Vx = Vy - Vx, set VF = NOT borrow. (8xy7 - SUBN Vx, Vy)
+                case 0x0007:
+
+                    if (V[y] > V[x])
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
+
+                    V[x] = V[x] - V[y];
+                    break;
+
+                //Set Vx = Vx SHL 1. (8xyE - SHL Vx {, Vy})
+                case 0x000E:
+
+                    if ((V[x] & 0x80) == 0x00)
+                        V[0xF] = 1;
+                    else
+                        V[0xF] = 0;
+
+                    V[x] <<= 1;
+                    break;
+
+                default:
+                    std::cerr << "Opcode doesnt supported: " << std::hex << opcode << std::endl;
+                    break;
+            }
+
+        // Skip next instruction if Vx != Vy (9xy0 - SNE Vx, Vy)
         case 0x9000:
+            if (V[x] != V[y])
+                PC += 2;
             break;
 
+        // Set I = nnn.(Annn - LD I, addr)
         case 0xA000:
             break;
 
@@ -193,10 +294,14 @@ void Chip8::LoopFDE() {
 
         case 0xF000:
             break;
+
+        default:
+            std::cerr << "Opcode doesnt supported: " << std::hex << opcode << std::endl;
+            break;
     }
 
     // Execute
-
+    PC+=2;
 
     // UpdateTimers
 
