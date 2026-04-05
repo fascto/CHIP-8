185048455488ba332740574dbbcb54981922f324
Fascto <fascto.05@gmail.com>
2026-04-05 20:01:57 +0000

    fix(refactor): Refactoring and isolating GUI from main part 1

--- src/chip8_gui.cpp
+++ src/chip8_gui.cpp
@@ -1,3 +1,36 @@
 //
 // Created by tdela on 5/4/2026.
 //
+
+#include "chip8_gui.h"
+
+Chip8Gui::Chip8Gui() {
+
+}
+
+Chip8Gui::~Chip8Gui() {
+
+}
+
+
+bool Chip8Gui::init() {
+
+}
+
+void Chip8Gui::update() {
+
+}
+
+
+void Chip8Gui::processInput() {
+
+}
+
+void Chip8Gui::render() {
+
+}
+
+void Chip8Gui::close() {
+
+}
+
--- src/chip8_gui.h
+++ src/chip8_gui.h
@@ -4,5 +4,34 @@
 
 #ifndef CHIP_8_CHIP8_GUI_H
 #define CHIP_8_CHIP8_GUI_H
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
+
+class Chip8Gui {
+
+private:
+
+    SDL_Window *gWindow;
+    SDL_Renderer *gRenderer;
+    SDL_Texture *gTexture;
+
+    uint32_t kScreenWidth = 640;
+    uint32_t kScreenHeight = 480;
+
+public:
+
+    Chip8Gui();
+    ~Chip8Gui();
+
+    Chip8Gui(const Chip8Gui &) = delete;
+    Chip8Gui &operator=(const Chip8Gui &) = delete;
+
+    bool init();
+    void update();
+    void processInput();
+    void render();
+    void close();
+
+};
 
 #endif //CHIP_8_CHIP8_GUI_H
